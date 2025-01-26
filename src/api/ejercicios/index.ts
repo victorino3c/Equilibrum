b569fc87819dbd39570e70e6909cc89cb32cf543
nicolas.victorino <nicolas.victorino@estudiante.uam.es>
2025-01-26 19:38:55 +0100

    Ahora se limpia correctamente la queryKey tras añadir el entrenamiento, mostrandose correctamente en el calendario directamente al terminar el entrenamiento

--- src/api/ejercicios/index.ts
+++ src/api/ejercicios/index.ts
@@ -86,5 +86,8 @@ export const useInsertEjerciciosEntrenamiento = () => {
 
       return data;
     },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['entrenamientos'] });
+    },
   });
 };
