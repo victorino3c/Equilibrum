8fed01bdc95c1ed7ab313f4b5a9c0551dec6d2d9
nicolas.victorino <nicolas.victorino@estudiante.uam.es>
2025-02-05 13:03:27 +0100

    Añadido estilos básicos para vista de detallesEjercicio.tsx. Falta por hacer esta vista casi por completo

--- src/app/(protected)/Ejercicio/DetallesEjercicio.tsx
+++ src/app/(protected)/Ejercicio/DetallesEjercicio.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 
 import { useGetEjercicioById } from '@api/ejercicios';
@@ -18,10 +18,23 @@ const DetallesEjercicio = () => {
   const { data: ejercicio, error, isLoading } = useGetEjercicioById(id);
 
   return (
-    <View>
-      <Text>detallesEjercicio</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>detallesEjercicio</Text>
     </View>
   );
 };
 
 export default DetallesEjercicio;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
